3d0d63586ccc0e42fa53cf6814e402c409a46645
will-r <will@spanner.org>
2019-10-18 14:30:11 +0100

    only bundle published pages

--- app/controllers/chemistry/pages_controller.rb
+++ app/controllers/chemistry/pages_controller.rb
@@ -141,10 +141,10 @@ module Chemistry
     # TODO soon to be another view of the Site object.
     #
     def bundle
-      if latest_page = Page.published_and_placeholders.latest.limit(1).first
+      if latest_page = Page.published.latest.limit(1).first
         expires_in(1.hour, public: true)
         if stale?(etag: latest_page, last_modified: latest_page.published_at, public: true)
-          @pages = Page.published_and_placeholders.includes(:sections, :socials, :image, :video)
+          @pages = Page.published.includes(:sections, :socials, :image, :video)
           return_pages_with_everything
         end
       end
