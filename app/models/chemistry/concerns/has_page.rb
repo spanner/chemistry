f37f53a4109f1d621f2e952bddddf639c1b371a4
Will Ross <will@spanner.org>
2021-02-03 12:54:55 +0000

    haspage means a belongs_to

--- app/models/chemistry/concerns/has_page.rb
+++ app/models/chemistry/concerns/has_page.rb
@@ -5,7 +5,7 @@ module Chemistry::Concerns::HasPage
     cattr_accessor :cms_page_collection
 
     def has_one_page(opts={})
-      has_one :page, class_name: "Chemistry::Page"
+      belongs_to :page, class_name: "Chemistry::Page"
       after_save :update_page
     end
   end
